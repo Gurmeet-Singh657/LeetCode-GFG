0a1b3cff947486dca9d983bde7c6304e4ccbd263
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2022-09-02 00:11:18 +0530

    Time: 151 ms (8.61%), Space: 302.6 MB (8.52%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int f(int curr,int n,int count)
+    int f(int curr,int n,int count,vector<vector<int>>& dp)
     {
         if(count==n)
             return 0;
         if(count>n)
             return 1e9;
-        
-        int firstway=1+f(curr,n,count+curr);
-        int secondway=2+f(count,n,count+count);
-        return min(firstway,secondway);
+        if(dp[curr][count]!=-1) return dp[curr][count];
+        int firstway=1+f(curr,n,count+curr,dp);
+        int secondway=2+f(count,n,count+count,dp);
+        return dp[curr][count]=min(firstway,secondway);
     }
     int minSteps(int n) {
         if(n==0) return 0;
         if(n==1) return 0;
-        return 1+f(1,n,1);
+        vector<vector<int>> dp(n+1,vector<int>(2*n+1,-1));
+        return 1+f(1,n,1,dp);
     }
 };
