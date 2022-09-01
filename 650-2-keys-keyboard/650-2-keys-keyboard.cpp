69e025704d8365644dcbd18001685ea83dd6a2a2
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2022-09-02 00:13:08 +0530

    Time: 141 ms (8.75%), Space: 302.5 MB (8.52%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -1,5 +1,24 @@
 class Solution {
 public:
+    // Recursion
+//     int f(int curr,int n,int count)
+//     {
+//         if(count==n)
+//             return 0;
+//         if(count>n)
+//             return 1e9;
+        
+//         int firstway=1+f(curr,n,count+curr);
+//         int secondway=2+f(count,n,count+count);
+//         return min(firstway,secondway);
+//     }
+//     int minSteps(int n) {
+//         if(n==0) return 0;
+//         if(n==1) return 0;
+//         return 1+f(1,n,1);
+//     }
+    
+    // Memoization
     int f(int curr,int n,int count,vector<vector<int>>& dp)
     {
         if(count==n)
