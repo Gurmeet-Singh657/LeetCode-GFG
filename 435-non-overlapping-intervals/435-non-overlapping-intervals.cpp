f6d5f2c0d53d83c81107e4fbc8f813623a11008d
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2022-09-03 01:56:55 +0530

    Time: 1025 ms (9.20%), Space: 89.8 MB (45.47%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
@@ -53,27 +53,22 @@ public:
     // }
     
     // O(nlogn) solution
+    static bool cmp(vector<int>& first,vector<int>& second)
+    {
+        return first[1]<second[1];
+    }
     int eraseOverlapIntervals(vector<vector<int>>& intervals)
     {
-        sort(intervals.begin(),intervals.end());
+        sort(intervals.begin(),intervals.end(),cmp);
         int n=intervals.size();
         int prev=0;
         int count=0;
         for(int start=1;start<n;start++)
         {
-            if(intervals[prev][1]<=intervals[start][0]) // Non - overlapping case
-            {
+           if(intervals[start][0]<intervals[prev][1])
+               count++;
+            else
                 prev=start;
-            }
-            else if(intervals[prev][1]<=intervals[start][1]) // Partial Overlapping 
-            {
-                count+=1;
-            }
-            else if(intervals[prev][1]>intervals[start][1]) // Containing whole
-            {
-                count+=1;
-                prev=start;
-            }
         }
         return count;
         
