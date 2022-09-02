68f3c473e62323501d5e82a2905093e652f2a2cd
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2022-09-03 01:57:30 +0530

    Time: 976 ms (13.24%), Space: 89.8 MB (81.20%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
@@ -1,57 +1,5 @@
 class Solution {
 public:
-    // Recursion
-    // int f(vector<vector<int>>& intervals,int ind,int n,int prev)
-    // {
-    //     if(ind==n)
-    //         return 0;
-    //     int nottake=f(intervals,ind+1,n,prev);
-    //     int take=INT_MIN;
-    //     if(prev==-1 || intervals[prev][1]<=intervals[ind][0])
-    //         take=1+f(intervals,ind+1,n,ind);
-    //     return max(take,nottake);
-    // }
-    // int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-    //     sort(intervals.begin(),intervals.end());
-    //     return intervals.size()-f(intervals,0,intervals.size(),-1);
-    // }
-    // Memoization
-    // int f(vector<vector<int>>& intervals,int ind,int n,int prev,vector<vector<int>>& dp)
-    // {
-    //     if(ind==n)
-    //         return 0;
-    //     if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
-    //     int nottake=f(intervals,ind+1,n,prev,dp);
-    //     int take=INT_MIN;
-    //     if(prev==-1 || intervals[prev][1]<=intervals[ind][0])
-    //         take=1+f(intervals,ind+1,n,ind,dp);
-    //     return dp[ind][prev+1]=max(take,nottake);
-    // }
-    // int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-    //     sort(intervals.begin(),intervals.end());
-    //     vector<vector<int>> dp(intervals.size(),vector<int>(intervals.size()+1,-1));
-    //     return intervals.size()-f(intervals,0,intervals.size(),-1,dp);
-    // }
-    // Tabulation
-    
-    // int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-    //     sort(intervals.begin(),intervals.end());
-    //     int n=intervals.size();
-    //     vector<vector<int>> dp(intervals.size()+1,vector<int>(intervals.size()+1,0));
-    //     for(int ind=n-1;ind>=0;ind--)
-    //     {
-    //         for(int prev=ind-1;prev>=-1;prev--)
-    //         {
-    //             int nottake=dp[ind+1][prev+1];
-    //             int take=INT_MIN;
-    //             if(prev==-1 || intervals[prev][1]<=intervals[ind][0])
-    //                 take=1+dp[ind+1][ind+1];
-    //             dp[ind][prev+1]=max(take,nottake);
-    //         }
-    //     }
-    //     return intervals.size()-dp[0][0];
-    // }
-    
     // O(nlogn) solution
     static bool cmp(vector<int>& first,vector<int>& second)
     {
