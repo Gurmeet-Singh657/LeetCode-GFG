3d6ccdeb41b5f96c6de42770139354cbd8b8f3f1
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2022-09-03 19:35:29 +0530

    Time: 8 ms (30.33%), Space: 7.3 MB (79.55%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    void f(int n,int k,vector<int>& ans,string str,int prev)
+    void f(int n,int k,vector<int>& ans,string str)
     {
         if(n==0)
         {
             ans.push_back(stoi(str));
             return;
         }
+        char ch=str[str.size()-1];
+        int prev=ch-'0';
         for(int i=0;i<=9;i++)
         {
             if(abs(prev-i)==k)
-                f(n-1,k,ans,str+to_string(i),i);
+                f(n-1,k,ans,str+to_string(i));
         }
         
     }
@@ -18,7 +20,7 @@ public:
         vector<int> ans;
         for(int i=1;i<=9;i++)
         {
-            f(n-1,k,ans,to_string(i),i);
+            f(n-1,k,ans,to_string(i));
         }
         return ans;
     }
