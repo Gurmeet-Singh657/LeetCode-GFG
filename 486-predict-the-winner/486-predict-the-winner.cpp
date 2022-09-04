0811aacbfb17e187129de1717208ce074ae4c2c5
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2022-09-05 03:57:03 +0530

    Time: 215 ms (20.88%), Space: 7.3 MB (97.20%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     int f(vector<int>& nums,int start,int end)
     {
-        if(start>end) return 0;
         if(start==end) return nums[start];
-        int ithtake=nums[start]+min(f(nums,start+1,end-1),f(nums,start+2,end));
-        int jthtake=nums[end]+min(f(nums,start+1,end-1),f(nums,start,end-2));
+        int ithtake=nums[start]-f(nums,start+1,end);
+        int jthtake=nums[end]-f(nums,start,end-1);
         return max(ithtake,jthtake);
     }
     bool PredictTheWinner(vector<int>& nums) {
-        int player1score=f(nums,0,nums.size()-1);
-        int totalscore=accumulate(nums.begin(),nums.end(),0);
-        return player1score>=totalscore-player1score;
+        return f(nums,0,nums.size()-1)>=0;
     }
 };
