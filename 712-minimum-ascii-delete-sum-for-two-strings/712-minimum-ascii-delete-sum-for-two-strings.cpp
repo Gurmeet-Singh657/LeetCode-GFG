bce6bd91f631ba129a016d36e310613a97c09390
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2022-09-04 19:13:17 +0530

    Time: 92 ms (22.75%), Space: 15.3 MB (41.03%) - LeetHub

--- 712-minimum-ascii-delete-sum-for-two-strings/712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -9,26 +9,32 @@ public:
         }
         return curr;
     }
-    int f(string& s,string& t,int i,int j,vector<vector<int>>& dp)
-    {
-        if(i==s.length() || j==t.length())
-        {
-            return findascii(s,i)+findascii(t,j);
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==t[j])
-            return dp[i][j]=f(s,t,i+1,j+1,dp);
-        else
-        {
-            return dp[i][j]=min((int)s[i]+f(s,t,i+1,j,dp),(int)t[j]+f(s,t,i,j+1,dp));
-        }   
-        return -1;
-    }
-    
     int minimumDeleteSum(string s1, string s2) {
         int n=s1.length();
         int m=s2.length();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return f(s1,s2,0,0,dp);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        // when i's are exhausted then all js will be taken
+        for(int j=0;j<m;j++)
+        {
+            dp[n][j]=findascii(s2,j);
+        }
+        // when js are exhausted then all is will be taken
+        for(int i=0;i<n;i++)
+        {
+            dp[i][m]=findascii(s1,i);
+        }
+        // when both are exhausted , so 0 is the answer
+        dp[n][m]=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=m-1;j>=0;j--)
+            {
+                if(s1[i]==s2[j])
+                    dp[i][j]=dp[i+1][j+1];
+                else
+                    dp[i][j]=min((int)(s1[i])+dp[i+1][j],(int)(s2[j])+dp[i][j+1]);
+            }
+        }
+        return dp[0][0];
     }
 };
