cbbe35121d202572dacef6227c57250a257c04f1
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2022-09-05 05:16:36 +0530

    Time: 14 ms (80.73%), Space: 6.5 MB (94.61%) - LeetHub

--- 688-knight-probability-in-chessboard/688-knight-probability-in-chessboard.cpp
+++ 688-knight-probability-in-chessboard/688-knight-probability-in-chessboard.cpp
@@ -3,12 +3,12 @@ public:
     int dx[8]={-2,-2,2,2,-1,-1,1,1};
     int dy[8]={-1,1,-1,1,-2,2,-2,2};
     double knightProbability(int n, int k, int row, int column) {
-        vector<vector<vector<double>>> dp(k+1,vector<vector<double>>(n+1,vector<double>(n+1,0)));
+        vector<vector<double>> curr(n+1,vector<double>(n+1,0)),prev(n+1,vector<double>(n+1,0));
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<n;j++)
             {
-                dp[0][i][j]=1.0;
+                prev[i][j]=1.0;
             }
         }
         for(int ki=1;ki<=k;ki++)
@@ -24,13 +24,14 @@ public:
                         int ccol=dy[ind]+j;
                         if(crow>=0 && crow<n && ccol>=0 && ccol<n)
                         {
-                            ans+=(0.125*dp[ki-1][crow][ccol]);
+                            ans+=(0.125*prev[crow][ccol]);
                         }
                     }
-                    dp[ki][i][j]=ans;
+                    curr[i][j]=ans;
                 }
             }
+            prev=curr;
         }
-        return dp[k][row][column];
+        return prev[row][column];
     }
 };
