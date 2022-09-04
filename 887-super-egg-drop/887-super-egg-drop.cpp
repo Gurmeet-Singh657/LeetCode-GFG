ca3fae8ede5fefaf7b743dc9da84d49d65d520d0
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2022-09-05 02:48:44 +0530

    Time: 847 ms (7.16%), Space: 47.3 MB (7.70%) - LeetHub

--- 887-super-egg-drop/887-super-egg-drop.cpp
+++ 887-super-egg-drop/887-super-egg-drop.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
-    int f(int n,int k,vector<vector<int>>& dp)
-    {
-        if(n==0 || n==1) return n;
-        if(k==1) return n;
-        
-        if(dp[n][k]!=-1) return dp[n][k];
-       
-        int minmove=1e9;
-        int start=1,end=n;
-        while(start<=end)
+    int superEggDrop(int k, int n) {
+        vector<vector<int>> dp(n+1,vector<int>(k+1,0));
+        for(int floor=1;floor<=n;floor++)
         {
-            int mid=(start+end)/2;
-            int eggnotbreak=f(n-mid,k,dp);
-            int eggbreak=f(mid-1,k-1,dp);
-            minmove=min(minmove,1+max(eggbreak,eggnotbreak));
-            if(eggbreak<eggnotbreak) // since we need more value in worst case
+            for(int egg=1;egg<=k;egg++)
             {
-                start=mid+1;
-            }
-            else
-            {
-                end=mid-1;
+                if(egg==1) dp[floor][egg]=floor;
+                else if(floor==1) dp[floor][egg]=1;
+                else
+                {
+                    int minmove=1e9;
+                    int start=1,end=floor;
+                    while(start<=end)
+                    {
+                        int mid=(start+end)/2;
+                        int eggnotbreak=dp[floor-mid][egg];
+                        int eggbreak=dp[mid-1][egg-1];
+                        minmove=min(minmove,1+max(eggbreak,eggnotbreak));
+                        if(eggbreak<eggnotbreak) // since we need more value in worst case
+                        {
+                            start=mid+1;
+                        }
+                        else
+                        {
+                            end=mid-1;
+                        }
+                    }
+                    dp[floor][egg]=minmove;
+                }
+                
             }
         }
-        return dp[n][k]=minmove;
-    }
-    int superEggDrop(int k, int n) {
-        vector<vector<int>> dp(n+1,vector<int>(k+1,-1));
-        return f(n,k,dp);
+        return dp[n][k];
     }
 };
