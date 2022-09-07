317dbad9d10d28ef2128419c5e68f6156f5fe1c7
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2022-09-07 18:19:51 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (51.15%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
-    bool f(string& s1,string& s2,string& s3,int i,int j,int k,vector<vector<int>>& dp)
-    {
-        if(k==s3.length())
-            return i==s1.length() && j==s2.length();
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(i<s1.length() && s1[i]==s3[k])
+        bool f(string &s1, string &s2, string &s3, int i, int j, vector<vector<int>> &dp)
         {
-            bool curr1=f(s1,s2,s3,i+1,j,k+1,dp);
-            if(curr1==true) return dp[i][j]=true;
+            if (i + j == s3.length())
+                return i == s1.length() && j == s2.length();
+            if (dp[i][j] != -1)
+                return dp[i][j];
+            if (i < s1.length() && s1[i] == s3[i + j])
+            {
+                bool curr1 = f(s1, s2, s3, i + 1, j, dp);
+                if (curr1 == true)
+                    return dp[i][j] = true;
+            }
+            if (j < s2.length() && s2[j] == s3[i + j])
+            {
+                bool curr2 = f(s1, s2, s3, i, j + 1, dp);
+                if (curr2 == true)
+                    return dp[i][j] = true;
+            }
+            return dp[i][j] = false;
         }
-        if(j<s2.length() && s2[j]==s3[k])
+        bool isInterleave(string s1, string s2, string s3)
         {
-            bool curr2=f(s1,s2,s3,i,j+1,k+1,dp);
-            if(curr2==true) return dp[i][j]=true;
+            if (s1.length() + s2.length() != s3.length())
+                return false;
+            int n = s1.length();
+            int m = s2.length();
+            int total = s3.length();
+            vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+            return f(s1, s2, s3, 0, 0, dp);
         }
-        return dp[i][j]=false;
-    }
-    bool isInterleave(string s1, string s2, string s3) {
-        if(s1.length()+s2.length()!=s3.length()) return false;
-        int n=s1.length();
-        int m=s2.length();
-        int total=s3.length();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return f(s1,s2,s3,0,0,0,dp);
-    }
 };
