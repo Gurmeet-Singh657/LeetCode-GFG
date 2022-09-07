467047d5ff909b097aa868119ebb3db9056f8c46
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2022-09-07 19:08:09 +0530

    Time: 67 ms (35.79%), Space: 10.4 MB (38.88%) - LeetHub

--- 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
+++ 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
@@ -3,24 +3,22 @@ public:
     int numSplits(string s) {
         int count=0;
         unordered_map<int,int> mp1;
-        unordered_map<int,int> mp2;
-        unordered_set<char> st;
+        vector<int> pre(s.length());
         for(int i=0;i<s.length();i++)
         {
-            st.insert(s[i]);
-            mp1[i]=st.size();
+            mp1[s[i]]++;
+            pre[i]=mp1.size();
         }
-        st.clear();
-        for(int i=s.length()-1;i>=0;i--)
+        int mycount=mp1.size();
+        mp1[s[0]]--;
+        if(mp1[s[0]]==0) mycount--;
+        for(int i=1;i<s.size();i++)
         {
-            st.insert(s[i]);
-            mp2[i]=st.size();
-        }
-        for(int i=1;i<s.length();i++)
-        {
-            int leftcnt=mp1[i-1];
-            int rightcnt=mp2[i];
-            if(leftcnt==rightcnt) count++;
+            if(mycount==pre[i-1])
+                count++;
+            mp1[s[i]]--;
+            if(mp1[s[i]]==0)
+                mycount--;
         }
         return count;
     }
