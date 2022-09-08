feb7f5bb4c61d76bbdb5cca0361ae386e89e2f13
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2022-09-09 04:28:02 +0530

    Time: 170 ms (9.18%), Space: 28.3 MB (15.62%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
-    void reachborder(vector<vector<int>>& grid,int i,int j,int& count)
+    void DFS(vector<vector<int>>& grid,int i,int j)
     {
         if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]==0)
             return;
-        
-        if(i==grid.size()-1 || i==0 || j==0 || j==grid[0].size()-1)
-            count=-1;
-        else if(count>=0)
-            count++;
         grid[i][j]=0;
         int dx[4]={-1,1,0,0};
         int dy[4]={0,0,-1,1};
@@ -16,26 +11,23 @@ public:
         {
             int curri=i+dx[k];
             int currj=j+dy[k];
-            reachborder(grid,curri,currj,count);
+            DFS(grid,curri,currj);
         }
     }
     int numEnclaves(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        int count=0;
         for(int i=0;i<grid.size();i++)
         {
             for(int j=0;j<grid[0].size();j++)
             {
-                if(grid[i][j]==1)
-                {
-                    int curr=0;
-                    reachborder(grid,i,j,curr);
-                    if(curr>0)
-                        count+=curr;
-                }
+                if(i*j==0 || i==grid.size()-1 || j==grid[0].size()-1)
+                    DFS(grid,i,j);
             }
         }
+        int count=0;
+        for(int i=0;i<grid.size();i++)
+            count+=accumulate(grid[i].begin(),grid[i].end(),0);
         return count;
     }
 };
