1707059ea0420fba3ff71d21cb8a65e3d990e278
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2022-09-12 23:55:48 +0530

    Time: 24 ms (5.36%), Space: 10.5 MB (94.44%) - LeetHub

---
+++ 948-bag-of-tokens/948-bag-of-tokens.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+       sort(tokens.begin(),tokens.end());
+       int i=0,j=tokens.size()-1;
+        int score=0;
+        int maxscore=0;
+        while(i<=j)
+        {
+            if(power>=tokens[i])
+            {
+                power-=tokens[i];
+                score++;
+                maxscore=max(maxscore,score);
+                i++;
+            }
+            else if(score>0)
+            {
+                power+=tokens[j];
+                j--;
+                score--;
+            }
+            else 
+            {
+                break;
+            }
+        }
+        return maxscore;
+    }
+};
