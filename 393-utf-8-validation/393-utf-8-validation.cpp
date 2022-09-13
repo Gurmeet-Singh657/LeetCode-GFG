51d4d3cde06b6f1287d5e0862254292ee94109b4
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2022-09-13 15:03:17 +0530

    Time: 36 ms (13.96%), Space: 14 MB (76.31%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool validUtf8(vector<int>& data) {
+       int count=0;
+        for(int i=0;i<data.size();i++)
+       {
+           int ele=data[i];
+           if(count==0)
+           {
+               if((ele>>5)==0b110)
+               {
+                   count=1;
+               }
+               else if((ele>>4)==0b1110)
+               {
+                   count=2;
+               }
+               else if((ele>>3)==0b11110)
+               {
+                   count=3;
+               }
+               else if(ele>>7) { // since 1 are more than 4 this means wrong sequence
+                   return false;
+               }
+           }
+            else
+            {
+                if((ele>>6)!=0b10)
+                    return false;
+                count--;
+            }
+       }
+        return (count==0);
+    }
+};
