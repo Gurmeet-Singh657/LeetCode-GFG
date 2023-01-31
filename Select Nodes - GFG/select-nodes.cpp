155f015d1059addc9f39485ec7b92f63b317db1d
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2023-01-31 21:31:07 +0530

    Added solution - LeetHub

--- Select Nodes - GFG/select-nodes.cpp
+++ Select Nodes - GFG/select-nodes.cpp
@@ -9,49 +9,35 @@ using namespace std;
 
 class Solution{
   public:
-  int count=0;
-  bool dfs(vector<int> adj[],vector<int> &visited,int node){
-      //mkae node visted
-      visited[node]=1;
-      bool select=false;
-      for(auto i:adj[node]){
-          if(visited[i]==0){
-              
-            //call the dfs for the child
-            bool isChildSelected=dfs(adj,visited,i);
-            
-            //means if child is not selected than select the parent
-            if(!isChildSelected){
-                select=true;
+    int count=0;
+    bool DFS(int node,vector<int> adj[],bool visited[])
+    {
+        bool selected=false;
+        visited[node]=true;
+        for(auto it:adj[node])
+        {
+            if(!visited[it])
+            {
+                // if from bottom node is not selected -> select now
+                if(!DFS(it,adj,visited))
+                {
+                    selected=true;
+                }
             }
-          }
-          
-      }
-      
-      //increment the count if the current node is selected
-      if(select){
-          count++;
-      }
-      return select;
-      
-  }
-  
-    int countVertex(int N, vector<vector<int>>edges){
-        
-        //making adjacency matrix
-        vector<int> adj[N+1];
-        for(auto i:edges){
-            int v=i[0];
-            int u=i[1];
-            adj[v].push_back(u);
-            adj[u].push_back(v);
         }
-        
-        //visted array
-        vector<int> visited(N+1,0);
-        dfs(adj,visited,1);
+        if(selected) count++;
+        return selected;
+    }
+    int countVertex(int N, vector<vector<int>>edges){
+        bool visited[N+1]={0};
+        vector<int> adj[N+1];
+        for(auto it:edges)
+        {
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+        DFS(1,adj,visited);
         return count;
-        
     }
 };
 
