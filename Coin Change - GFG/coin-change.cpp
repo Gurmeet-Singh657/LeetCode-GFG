1829432bc1d4b882996759f13a4212b0016cd9f9
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2023-03-11 01:05:57 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.cpp
+++ Coin Change - GFG/coin-change.cpp
@@ -5,32 +5,25 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    long long f(int ind,int coins[],int N,int sum,vector<vector<long long int>>& dp)
-    {
-        if(sum==0) return 1;
-        if(ind==N) return 0;
-        if(dp[ind][sum]!=-1) return dp[ind][sum];
-        long long nottake=f(ind+1,coins,N,sum,dp);
-        long long take=0;
-        if(sum>=coins[ind])
-            take=f(ind,coins,N,sum-coins[ind],dp);
-        return dp[ind][sum]=take+nottake;
-    }
+    #define ll long long int
     long long int count(int coins[], int N, int sum) {
-        vector<vector<long long int>> dp(N+1,vector<long long int>(sum+1,0));
-        for(int i=0;i<N;i++) dp[i][0]=1;
+        vector<ll> ahead(sum+1,0);
+        ahead[0]=1;
         for(int ind=N-1;ind>=0;ind--)
         {
+            vector<ll> curr(sum+1,0);
+            curr[0]=1;
             for(int s=1;s<=sum;s++)
             {
-                long long nottake=dp[ind+1][s];
+                long long nottake=ahead[s];
                 long long take=0;
                 if(s>=coins[ind])
-                    take=dp[ind][s-coins[ind]];
-                dp[ind][s]=take+nottake;
+                    take=curr[s-coins[ind]];
+                curr[s]=take+nottake;
             }
+            ahead=curr;
         }
-        return dp[0][sum];
+        return ahead[sum];
     }
 };
 
