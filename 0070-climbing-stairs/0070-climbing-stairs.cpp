d885ed322a338d2e013c83a053c942c7025a6b1a
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2023-03-11 04:19:05 +0530

    Time: 4 ms (9.21%), Space: 6.3 MB (19.77%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -7,7 +7,12 @@ public:
         return dp[n]=f(n-1,dp)+f(n-2,dp);
     }
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return f(n,dp);
+        vector<int> dp(n+1,0);
+        dp[0]=dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
