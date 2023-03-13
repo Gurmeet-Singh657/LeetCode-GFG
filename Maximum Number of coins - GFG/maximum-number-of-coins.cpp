6332cea4d8595d5fccf513596a27ab17ab3079a5
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2023-03-14 00:07:04 +0530

    Added solution - LeetHub

--- Maximum Number of coins - GFG/maximum-number-of-coins.cpp
+++ Maximum Number of coins - GFG/maximum-number-of-coins.cpp
@@ -22,8 +22,20 @@ class Solution{
         {
             a.insert(a.begin(),1);
             a.push_back(1);
-            vector<vector<int>> dp(N+1,vector<int>(N+1,-1));
-            return f(1,N,a,dp);
+            vector<vector<int>> dp(N+2,vector<int>(N+1,0));
+            for(int i=N;i>=1;i--)
+            {
+                for(int j=i;j<=N;j++)
+                {
+                    int ans=-1e9;
+                    for(int k=i;k<=j;k++)
+                    {
+                        ans=max(ans,(a[i-1]*a[k]*a[j+1])+dp[i][k-1]+dp[k+1][j]);
+                    }
+                    dp[i][j]=ans;
+                }
+            }
+            return dp[1][N];
         }
 };
 
