c401527d4b02130ac67be41cca661897ad965c4a
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2023-03-22 03:35:58 +0530

    Added solution - LeetHub

--- Optimal Strategy For A Game - GFG/optimal-strategy-for-a-game.cpp
+++ Optimal Strategy For A Game - GFG/optimal-strategy-for-a-game.cpp
@@ -16,8 +16,17 @@ class Solution{
         return dp[i][j]=max(first,second);
     }
     long long maximumAmount(int arr[], int n){
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        return f(0,n-1,arr,dp);
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=i;j<n;j++)
+            {
+                int first=arr[i]+min((i+1<n && j-1>=0?dp[i+1][j-1]:1e9),(i+2<n?dp[i+2][j]:1e9));
+                int second=arr[j]+min((i+1<n && j-1>=0?dp[i+1][j-1]:1e9),(j-2>=0?dp[i][j-2]:1e9));
+                dp[i][j]=max(first,second);
+            }
+        }
+        return dp[0][n-1];
     }
 };
 
