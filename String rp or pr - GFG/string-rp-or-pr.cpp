343e45e3c84d02c89851f96c64c27dd0c3dd03f7
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2023-03-22 00:19:11 +0530

    Added solution - LeetHub

---
+++ String rp or pr - GFG/string-rp-or-pr.cpp
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution{   
+public:
+    long long solve(int X,int Y,string S){
+      stack<char> st;
+      int n=S.length();
+      long long ans=0;
+      for(int i=0;i<n;i++)
+      {
+          if(S[i]!='r' && S[i]!='p')
+          {
+             if(!st.empty())
+             {
+                 char ch=st.top();
+                 int countr=0,countp=0;
+                 while(!st.empty())
+                 {
+                     if(st.top()=='r') countr++;
+                     else countp++;
+                     st.pop();
+                 }
+                 if(ch=='r') ans+=X*min(countr,countp);
+                 if(ch=='p') ans+=Y*min(countr,countp);
+             }
+             continue;
+          }
+          if(st.empty())
+            st.push(S[i]);
+          else if(X>=Y)
+          {
+              if(st.top()=='p' && S[i]=='r')
+              {
+                  ans+=X;
+                  st.pop();
+              }
+              else
+              {
+                  st.push(S[i]);
+              }
+          }
+          else 
+          {
+              if(st.top()=='r' && S[i]=='p')
+              {
+                  ans+=Y;
+                  st.pop();
+              }
+              else
+              {
+                  st.push(S[i]);
+              }
+          }
+      }
+      if(!st.empty())
+         {
+             char ch=st.top();
+             int countr=0,countp=0;
+             while(!st.empty())
+             {
+                 if(st.top()=='r') countr++;
+                 else countp++;
+                 st.pop();
+             }
+             if(ch=='r') ans+=X*min(countr,countp);
+             if(ch=='p') ans+=Y*min(countr,countp);
+         }
+         return ans;
+    }
+};
+
+//{ Driver Code Starts.
+signed main()
+{
+  int t;
+  cin>>t;
+  while(t--)
+  {
+      int X,Y;
+      cin>>X>>Y;
+      string S;
+      cin>>S;
+      Solution obj;
+      long long answer=obj.solve(X,Y,S);
+      cout<<answer<<endl;
+  }
+}
+// } Driver Code Ends
