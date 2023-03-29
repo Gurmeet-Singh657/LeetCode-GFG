eb34e4b88ad7ad76696df9f0b087481e642c55c9
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2023-03-29 23:10:47 +0530

    Added solution - LeetHub

--- Longest alternating subsequence - GFG/longest-alternating-subsequence.cpp
+++ Longest alternating subsequence - GFG/longest-alternating-subsequence.cpp
@@ -7,15 +7,27 @@ class Solution {
 	public:
 		int AlternatingaMaxLength(vector<int>&nums){
 		    int n=nums.size();
-		    int inc=1,dec=1;
+		    vector<vector<int>> dp(n,vector<int>(2,0));
+		    dp[0][0]=dp[0][1]=1;
 		    for(int i=1;i<n;i++)
 		    {
 		            if(nums[i]>nums[i-1])
-		                inc=dec+1;
+		            {
+		                dp[i][1]=1+dp[i-1][0]; // Take previous dec and add this to answer
+		                dp[i][0]=dp[i-1][0]; // pass this dec ahead so that it can be used
+		            }
 		            if(nums[i]<nums[i-1])
-		                dec=inc+1;
+		            {
+		                dp[i][0]=1+dp[i-1][1]; // Take previous inc and add this to answer
+		                dp[i][1]=dp[i-1][1]; // pass this inc ahead so that it can be used
+		            }
+		            if(nums[i]==nums[i-1])
+		            {
+		                dp[i][0]=dp[i-1][0];
+		                dp[i][1]=dp[i-1][1];
+		            }
 		    }
-		    return max(inc,dec);
+		    return max(dp[n-1][0],dp[n-1][1]);
 		}
 
 };
