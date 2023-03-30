37d041cda34c8277d7dcc1da97cb80c8ad19bed2
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2023-03-30 16:16:13 +0530

    Added solution - LeetHub

--- Palindromic Partitioning - GFG/palindromic-partitioning.cpp
+++ Palindromic Partitioning - GFG/palindromic-partitioning.cpp
@@ -20,24 +20,23 @@ public:
         }
         return true;
     }
-    int f(int ind,string& str,vector<int>& dp)
-    {
-        if(ind==str.length()) return -1;
-        string curr="";
-        int mini=1e9;
-        if(dp[ind]!=-1) return dp[ind];
-        for(int i=ind;i<str.length();i++)
-        {
-            curr.push_back(str[i]);
-            if(palindrome(curr)) mini=min(mini,1+f(i+1,str,dp)); 
-        }
-        return dp[ind]=mini;
-    }
     int palindromicPartition(string str)
     {
         int n=str.length();
-        vector<int> dp(n,-1);
-        return f(0,str,dp);
+        vector<int> dp(n+1,0);
+        dp[n]=-1;
+        for(int ind=n-1;ind>=0;ind--)
+        {
+            int mini=1e9;
+            string curr="";
+            for(int i=ind;i<n;i++)
+            {
+                curr.push_back(str[i]);
+                if(palindrome(curr)) mini=min(mini,1+dp[i+1]); 
+            }
+           dp[ind]=mini;
+        }
+        return dp[0];
     }
 };
 
