3b747b1711807b0ab17872c1f1dc205322440bcc
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2023-04-06 02:23:02 +0530

    Added solution - LeetHub

--- Equal Left and Right Subarray Sum - GFG/equal-left-and-right-subarray-sum.cpp
+++ Equal Left and Right Subarray Sum - GFG/equal-left-and-right-subarray-sum.cpp
@@ -34,13 +34,12 @@ class Solution {
         int sum=0;
         for(int i=0;i<N;i++) sum+=A[i];
         
-        int aheadSum=sum;
-        int prevsum=0;
+        int aheadSum=sum,prevSum=0;
         for(int i=0;i<N;i++)
         {
             aheadSum-=A[i];
-            if(prevsum==aheadSum) return i+1;
-            prevsum+=A[i];
+            if(prevSum==aheadSum) return i+1; // 1based indexing
+            prevSum+=A[i];
         }
         return -1;
     }
