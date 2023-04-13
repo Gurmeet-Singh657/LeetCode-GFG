12d4889e1270336dfe85592600fc0d161b4f40ab
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2023-04-13 20:08:06 +0530

    Added solution - LeetHub

--- Partition the Array - GFG/partition-the-array.cpp
+++ Partition the Array - GFG/partition-the-array.cpp
@@ -11,37 +11,44 @@ public:
     pair<long long,long long> solve(long long presum[],int start,int end)
     {
         int low=start,high=end-1;
-        long long leftans=0,rightans=presum[end]-(start-1>=0?presum[start-1]:0);
+        long long leftans=0,rightans=1e9;
         while(low<=high)
         {
             int mid=(low+high)/2;
-            long long leftsum=presum[mid]-(start-1>=0?presum[start-1]:0),rightsum=presum[end]-presum[mid];
-            if(abs(leftsum-rightsum)<abs(leftans-rightans))
+            long long leftsum=presum[mid]-(start-1>=0?presum[start-1]:0);
+            long long rightsum=presum[end]-presum[mid];
+            if(abs(rightans-leftans)>abs(rightsum-leftsum))
             {
-                leftans=leftsum;
                 rightans=rightsum;
+                leftans=leftsum;
             }
-            if(leftsum<rightsum) low=mid+1;
-            else high=mid-1;
+            if(leftsum<rightsum)
+            {
+                low=mid+1;
+            }
+            else
+            {
+                high=mid-1;
+            }
         }
         return {leftans,rightans};
     }
-    long long minDifference(int N, vector<int> &A) {
+    long long minDifference(int N, vector<int> &arr) {
         long long presum[N]={0};
         for(int i=0;i<N;i++)
         {
             if(i>0) presum[i]+=presum[i-1];
-            presum[i]+=A[i];
+            presum[i]+=arr[i];
         }
         long long minans=1e9;
         for(int i=1;i<=N-3;i++)
         {
             auto fs=solve(presum,0,i);
-            auto tf=solve(presum,i+1,N-1);
+            auto ss=solve(presum,i+1,N-1);
             long long first=fs.first;
             long long second=fs.second;
-            long long third=tf.first;
-            long long fourth=tf.second;
+            long long third=ss.first;
+            long long fourth=ss.second;
             long long maxi=max(max(first,second),max(third,fourth));
             long long mini=min(min(first,second),min(third,fourth));
             minans=min(minans,maxi-mini);
