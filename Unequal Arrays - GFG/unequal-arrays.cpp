b89d0ff677dc55850faa357b84350da03d3116e4
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2023-04-16 01:35:27 +0530

    Added solution - LeetHub

--- Unequal Arrays - GFG/unequal-arrays.cpp
+++ Unequal Arrays - GFG/unequal-arrays.cpp
@@ -31,20 +31,20 @@ public:
 class Solution {
   public:
     long long solve(int N, vector<int> &A, vector<int> &B) {
-        sort(A.begin(),A.end());
-        sort(B.begin(),B.end());
-        vector<int> Aodds,Bodds;
-        vector<int> Aevens,Bevens;
+        sort(A.begin(),A.end()); // closer element should remain closer 
+        sort(B.begin(),B.end()); // Minimum opern
+        vector<int> Aodds,Aevens,Bodds,Bevens;
         long long sum1=0,sum2=0;
         for(int i=0;i<N;i++)
         {
+            sum1+=A[i];
+            sum2+=B[i];
             if(A[i]%2!=0) Aodds.push_back(A[i]);
             else Aevens.push_back(A[i]);
             if(B[i]%2!=0) Bodds.push_back(B[i]);
             else Bevens.push_back(B[i]);
-            sum1+=A[i];
-            sum2+=B[i];
         }
+        // confirmation that A->B
         if(sum1!=sum2 || Aodds.size()!=Bodds.size()) return -1;
         
         long long ans=0;
