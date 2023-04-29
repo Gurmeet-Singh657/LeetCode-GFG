b94abdaaf8bf776022bc715a0169891c76470fa9
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2023-04-30 01:35:08 +0530

    Added solution - LeetHub

---
+++ Powerfull Integer - GFG/powerfull-integer.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int powerfullInteger(int n,vector<vector<int>> &intervals,int k){
+        map<int,int> mp;
+        for(auto it:intervals)
+        {
+            int l=it[0],r=it[1];
+            mp[l]++;
+            mp[r+1]--;
+        }
+        int curr=0;
+        int maxi=-1;
+        bool flag=false;
+        for(auto it:mp)
+        {
+            curr+=it.second;
+            if(curr>=k)
+            {
+                flag=true;
+                maxi=it.first; 
+            }
+            else if(flag==true)
+            {
+                maxi=it.first-1;
+                flag=false;
+            }
+            else
+            {
+                flag=false;
+            }
+        }
+        return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n;
+        vector<vector<int>> intervals(n,vector<int>(2));
+        for(int i=0;i<n;i++){
+            cin>>intervals[i][0]>>intervals[i][1];
+        }
+        cin>>k;
+        Solution ob;
+        cout<<ob.powerfullInteger(n,intervals,k)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
