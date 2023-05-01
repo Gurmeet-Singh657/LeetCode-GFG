3e40faa311d0194f2458a3e067769c2de65d1034
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2023-05-02 01:42:26 +0530

    Added solution - LeetHub

--- Cake Distribution Problem - GFG/cake-distribution-problem.cpp
+++ Cake Distribution Problem - GFG/cake-distribution-problem.cpp
@@ -11,37 +11,37 @@ using namespace std;
 class Solution{
     public:
     #define ll long long int
-    bool check(int mid,vector<int>& sweetness,int N,int K)
+    bool check(ll mid,vector<int>& arr,int N,int K)
     {
-        int sum=0;
+        ll sum=0;
         int count=0;
+        // Aim - Number of pieces > = k
         for(int i=0;i<N;i++)
         {
-            sum+=sweetness[i];
+            sum+=arr[i];
             if(sum>=mid)
             {
                 count++;
                 sum=0;
             }
         }
-        if(sum>=mid) count++;
         return count>=K+1;
     }
     int maxSweetness(vector<int>& sweetness, int N, int K) {
-        ll start=1,end=(ll)1e14;
+        ll start=1,end=(int)(1e14);
         ll ans=-1;
         while(start<=end)
         {
-          ll mid=(start+end)/2LL;
-          if(check(mid,sweetness,N,K))
-          {
-              ans=mid;
-              start=mid+1;
-          }
-          else
-          {
-              end=mid-1;
-          }
+            ll mid=(start+end)/2;
+            if(check(mid,sweetness,N,K))
+            {
+                ans=mid;
+                start=mid+1;
+            }
+            else
+            {
+                end=mid-1;
+            }
         }
         return ans;
     }
