503a6ac2eb6bf8efa2b9805925636dea61a36722
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2023-06-26 00:46:52 +0530

    Added solution - LeetHub

--- nCr - GFG/n-cr.cpp
+++ nCr - GFG/n-cr.cpp
@@ -10,18 +10,18 @@ using namespace std;
 class Solution{
 public:
     // Memoisation
-    #define mod (int)(1e9+7)
-    int f(int n,int r,vector<vector<int>>& dp)
-    {
-        if(n<r) return 0;
-        if(r==0) return 1;
-        if(dp[n][r]!=-1) return dp[n][r];
-        return dp[n][r]=(f(n-1,r-1,dp)+0LL+f(n-1,r,dp))%mod;
-    }
-    int nCr(int n, int r){
-        vector<vector<int>> dp(n+1,vector<int>(r+1,-1));
-        return f(n,r,dp);
-    }
+    // #define mod (int)(1e9+7)
+    // int f(int n,int r,vector<vector<int>>& dp)
+    // {
+    //     if(n<r) return 0;
+    //     if(r==0) return 1;
+    //     if(dp[n][r]!=-1) return dp[n][r];
+    //     return dp[n][r]=(f(n-1,r-1,dp)+0LL+f(n-1,r,dp))%mod;
+    // }
+    // int nCr(int n, int r){
+    //     vector<vector<int>> dp(n+1,vector<int>(r+1,-1));
+    //     return f(n,r,dp);
+    // }
     // Tabulation
     // #define mod (int)(1e9+7)
     // int nCr(int n, int r){
@@ -40,6 +40,24 @@ public:
     //     }
     //     return dp[n][r];
     // }
+    // Space Optimisation
+    #define mod (int)(1e9+7)
+    int nCr(int n, int r){
+        if(n<r) return 0;
+        
+        vector<int> prev(n+1,0),curr(n+1,0);
+        prev[0]=1;
+        curr[0]=1;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                curr[j]=(prev[j-1]+0LL+prev[j])%mod;
+            }
+            prev=curr;
+        }
+        return curr[r];
+    }
 };
 
 //{ Driver Code Starts.
