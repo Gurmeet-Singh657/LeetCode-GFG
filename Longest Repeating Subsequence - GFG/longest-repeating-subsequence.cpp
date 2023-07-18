d31f1dc1a1494468b5673e2b098890632e2c14f4
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2023-07-18 11:16:13 +0530

    Added solution - LeetHub

--- Longest Repeating Subsequence - GFG/longest-repeating-subsequence.cpp
+++ Longest Repeating Subsequence - GFG/longest-repeating-subsequence.cpp
@@ -5,35 +5,26 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
 	public:
-	    int f(int i,int j,int n,string& str1,string& str2,vector<vector<int>>& dp)
+	    int LCS(int i,int j,string& str,vector<vector<int>>& dp)
 	    {
-	        if(i==n || j==n) return 0;
+	        if(i==str.length() || j==str.length()) return 0;
+	        
 	        if(dp[i][j]!=-1) return dp[i][j];
-	        int ans=1e9;
-	        if(str1[i]==str2[j] && i!=j)
-	           ans=1+f(i+1,j+1,n,str1,str2,dp);
+	        int ans;
+	        if(i!=j && str[i]==str[j])
+	        {
+	            ans=1+LCS(i+1,j+1,str,dp);
+	        }
 	        else
-	           ans=max(f(i+1,j,n,str1,str2,dp),f(i,j+1,n,str1,str2,dp));
-	       return dp[i][j]=ans;
+	        {
+	            ans=max(LCS(i+1,j,str,dp),LCS(i,j+1,str,dp));
+	        }
+	        return dp[i][j]=ans;
 	    }
 		int LongestRepeatingSubsequence(string str){
-		    string str1=str;
-		    string str2=str;
 		    int n=str.length();
-		    vector<vector<int>> dp(n+1,vector<int>(n+1,0));
-		    for(int i=n-1;i>=0;i--)
-		    {
-		        for(int j=n-1;j>=0;j--)
-		        {
-		            int ans=1e9;
-        	        if(str1[i]==str2[j] && i!=j)
-        	           ans=1+dp[i+1][j+1];
-        	        else
-        	           ans=max(dp[i+1][j],dp[i][j+1]);
-        	       dp[i][j]=ans;
-		        }
-		    }
-		    return dp[0][0];
+		    vector<vector<int>> dp(n,vector<int>(n,-1));
+		    return LCS(0,0,str,dp);
 		}
 
 };
