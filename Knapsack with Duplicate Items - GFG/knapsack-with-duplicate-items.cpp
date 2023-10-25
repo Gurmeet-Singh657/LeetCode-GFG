343e1d9d8bd428ea013eb52c472f2cda15688cf6
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2023-10-25 14:51:58 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -9,21 +9,23 @@ using namespace std;
 
 class Solution{
 public:
+    int solve(int ind,int val[],int wt[],int N,int W,vector<vector<int>>& dp)
+    {
+        if(ind==N) return 0;
+        if(dp[ind][W]!=-1) return dp[ind][W];
+        
+        int notTake=solve(ind+1,val,wt,N,W,dp);
+        int take=-1e9;
+        if(wt[ind]<=W)
+        {
+            take=val[ind]+solve(ind,val,wt,N,W-wt[ind],dp);
+        }
+        return dp[ind][W]=max(take,notTake);
+    }
     int knapSack(int N, int W, int val[], int wt[])
     {
-        vector<vector<int>> dp(N+1,vector<int>(W+1,0));
-        for(int weight=0;weight<=W;weight++)
-        {
-            for(int ind=N-1;ind>=0;ind--)
-            {
-                int nottake=dp[ind+1][weight];
-                int take=-1e9;
-                if(wt[ind]<=weight)
-                    take=val[ind]+dp[ind][weight-wt[ind]];
-                dp[ind][weight]=max(take,nottake);
-            }
-        }
-        return dp[0][W];
+        vector<vector<int>> dp(N,vector<int>(W+1,-1));
+        return solve(0,val,wt,N,W,dp);
     }
 };
 
